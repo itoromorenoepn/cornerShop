d53d00ddba260a207ba578cc1c1c1226db559c87
Jersson-Andrango <56616282+Jersson-Andrango@users.noreply.github.com>
2019-12-04 09:53:45 -0500

    login
    login realizado

--- app/src/main/java/is/epn/cornershop/MapsActivity1.java
+++ app/src/main/java/is/epn/cornershop/MapsActivity1.java
@@ -10,7 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity1 extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity1 extends FragmentActivity {
 
     private GoogleMap mMap;
 
